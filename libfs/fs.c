7560ec791c33045707146fd3d341e9c3d452708f
Cameron <cameronbrown33@gmail.com>
2020-02-25 21:19:38 -0800

    "make" now works

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,6 +7,11 @@
 #include "disk.h"
 #include "fs.h"
 
+#define EXIT_NOERR 0
+#define EXIT_ERR -1
+
+#define UNUSED(x) (void)(x)
+
 struct __attribute__((__packed__)) super_block {
 	int32_t signiture[2];
 	int16_t block_total;
@@ -18,7 +23,7 @@ struct __attribute__((__packed__)) super_block {
 };
 
 struct __attribute__((__packed__)) FAT {
-	int16_t* block_table;
+	int16_t** block_table;
 	int16_t length;
 };
 
@@ -27,67 +32,93 @@ struct __attribute__((__packed__)) root {
 	int32_t file_size;
 	int16_t data_index;
 	int8_t padding[10];
-}
+};
 
 /* TODO: Phase 1 */
 
 int fs_mount(const char *diskname)
 {
 	/* TODO: Phase 1 */
+	UNUSED(diskname);
+	return EXIT_NOERR;
 }
 
 int fs_umount(void)
 {
 	/* TODO: Phase 1 */
+	return EXIT_NOERR;
 }
 
 int fs_info(void)
 {
 	/* TODO: Phase 1 */
+	return EXIT_NOERR;
 }
 
 int fs_create(const char *filename)
 {
 	/* TODO: Phase 2 */
+	UNUSED(filename);
+	return EXIT_NOERR;
 }
 
 int fs_delete(const char *filename)
 {
 	/* TODO: Phase 2 */
+	UNUSED(filename);
+	return EXIT_NOERR;
 }
 
 int fs_ls(void)
 {
 	/* TODO: Phase 2 */
+	return EXIT_NOERR;
 }
 
 int fs_open(const char *filename)
 {
 	/* TODO: Phase 3 */
+	UNUSED(filename);
+	return EXIT_NOERR;
 }
 
 int fs_close(int fd)
 {
 	/* TODO: Phase 3 */
+	UNUSED(fd);
+	return EXIT_NOERR;
 }
 
 int fs_stat(int fd)
 {
 	/* TODO: Phase 3 */
+	UNUSED(fd);
+	return EXIT_NOERR;
 }
 
 int fs_lseek(int fd, size_t offset)
 {
 	/* TODO: Phase 3 */
+	UNUSED(fd);
+	UNUSED(offset);
+	return EXIT_NOERR;
 }
 
 int fs_write(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
+	UNUSED(fd);
+	UNUSED(buf);
+	UNUSED(count);
+	return EXIT_NOERR;
 }
 
 int fs_read(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
+	UNUSED(fd);
+	UNUSED(buf);
+	UNUSED(count);
+	return EXIT_NOERR;
 }
 
